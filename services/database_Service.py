6638ea75e2a6028dd741a29cabb8cf14c9f97895
mrsibatuaras <sibatuarachristian@gmail.com>
2022-12-15 03:21:21 +0700

    kai kai

--- services/database_Service.py
+++ services/database_Service.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 
-db_user = "root" # ex : root
-db_password = ""
-db_host = "localhost" # ex : localhost
-db_port = 3306 # ex :3306
-db_database = "tstkai" #ex : db_afkar
+db_user = "doadmin" # ex : root
+db_password = "AVNS_mVez6y_ykgMWWtrI_hr"
+db_host = "eagen-love-tst-do-user-10225549-0.b.db.ondigitalocean.com" # ex : localhost
+db_port = 25060 # ex :3306
+db_database = "kai" #ex : db_afkar
 db_sslmode = True
 
 # ini klo pake mysql
